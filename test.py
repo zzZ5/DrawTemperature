328c1d975bd2e4827ca138db61ac4780d77019e8
zzZ5 <baoju_liu@foxmail.com>
2022-12-21 16:23:17 +0800

    最新

--- test.py
+++ test.py
@@ -5,12 +5,13 @@ import seaborn
 import matplotlib.dates as mdates
 from matplotlib import pyplot as plt
 import matplotlib
+from matplotlib.pyplot import MultipleLocator
 
 plt.rcParams['font.family'] = 'SimHei'
 plt.rcParams['axes.unicode_minus'] = False
 
-path_pre = 'data2'
-for i in range(1, 6):
+path_pre = 'data1'
+for i in range(4, 6):
     data = pandas.read_csv(path_pre + '/' + 'mr' + str(i) + '.csv')
     co2 = pandas.read_csv(path_pre + '/' + 'CO2-' + str(i) + '.csv')
     data['newindex'] = np.arange(len(data)-1, -1, -1)
@@ -50,8 +51,9 @@ for i in range(1, 6):
                      yerr=data['Std'][::],  label='温度')
     handle1, label1 = ax.get_legend_handles_labels()
     ax.xaxis.set_major_formatter(plt.FuncFormatter(format_func))
-    ax.xaxis.set_major_locator(mdates.HourLocator(byhour=12))
+    ax.xaxis.set_major_locator(mdates.HourLocator(byhour=22))
     # ax.set_xticks([0, 1, 2, 3, 4])
+    ax.set_ylim((0, 80))
     ax.set_ylabel('温度(℃)')
 
     ax1 = ax.twinx()
@@ -61,15 +63,20 @@ for i in range(1, 6):
     # ax1.xaxis.set_major_formatter(plt.FuncFormatter(format_func))
     # ax1.xaxis.set_major_locator(mdates.HourLocator(interval=24))
     ax1.set_ylabel("氧气消耗速率(ppm/min)")
-    ax1.set_ylim((0, 20000000))
+    ax1.set_ylim((0, 12000000))
+    y_major_locator=MultipleLocator(1000000)
+    ax1.yaxis.set_major_locator(y_major_locator)
+
     ax.set_xlabel('时间(d)')
 
     if path_pre == 'data1':
-        plt.xlim((19290.91, 19298.11))
+        # plt.xlim((19290.91, 19298.11))
+        # plt.xlim((19290.91, 19297.68))
+        plt.xlim((19290.91, 19297.08))
 
     if path_pre == 'data2':
-        # plt.xlim((19308.913, 19315.97))
-        plt.xlim((19309.5, 19316.7))
+        # plt.xlim((19309.5, 19316.7))
+        plt.xlim((19309.5, 19315.66))
 
     if path_pre == 'data3':
         plt.xlim((19317.913, 19324.97))
